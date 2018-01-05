74aed90d971ede0564f83303e8fa0c7b0804eed0
Theo <theo.ryffel@hotmail.fr>
2018-01-05 18:34:33 +0100

    Reorganize 1st page and add box page

---
+++ src/TVF/StoreBundle/Controller/BoxController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace TVF\StoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class BoxController extends Controller
+{
+    /*
+      All basic pages related to the box offers
+    */
+    public $entityNameSpace = 'TVFStoreBundle:Box';
+
+    public function discoverAction()
+    {
+        $text = [];
+        $this->handler = $this->container->get('tvf_store.fieldhandler');
+        $text['amateur']['title'] = $this->handler->fetchText('amateur:title');
+        $text['amateur']['price'] = $this->handler->fetchText('amateur:price');
+        $text['amateur']['content'] = $this->handler->fetchText('amateur:content');
+        $text['amateur']['image'] = $this->handler->fetchImage('amateur:image');
+        $text['connaisseur']['title'] = $this->handler->fetchText('connaisseur:title');
+        $text['connaisseur']['price'] = $this->handler->fetchText('connaisseur:price');
+        $text['connaisseur']['content'] = $this->handler->fetchText('connaisseur:content');
+        $text['connaisseur']['image'] = $this->handler->fetchImage('connaisseur:image');
+        return $this->render($this->entityNameSpace.':discover.html.twig', array(
+          'data' => $text
+        ));
+    }
+}
